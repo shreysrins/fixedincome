4c71a68f10c77d4a6df81379256fb410e93151b1
Shreyas Srinivasan <shreyass@mit.edu>
2022-02-03 15:56:24 -0500

    Add initial package documentation

---
+++ fixedincome/__init__.py
@@ -0,0 +1,6 @@
+"""
+Fixed Income analytics
+
+This package was developed for 15.438 Fixed Income Securities and Derivatives, as taught by Professor Deborah J. Lucas at the MIT Sloan School of Management. Its recommended usage is as
+    >>> import fixedincome as fi
+"""
