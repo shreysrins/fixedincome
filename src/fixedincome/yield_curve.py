88e12686601c88e61b59df4be52d5aa64aa87999
Shreyas Srinivasan <shreyass@mit.edu>
2022-02-17 13:52:30 -0500

    Create fi.yield_curve module

---
+++ src/fixedincome/yield_curve.py
@@ -0,0 +1,21 @@
+"""
+Fixed income analytics for the yield curve
+
+Implements various functions useful for yield curve analytics.
+"""
+
+
+def bootstrap():
+	pass
+
+
+def regression():
+	pass
+
+
+def spline():
+	pass
+
+
+def nelson_siegel():
+	pass
