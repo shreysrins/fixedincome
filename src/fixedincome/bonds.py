ca6229f9eb0da6d16629d7a98c8ff4c0dfecd36d
Shreyas Srinivasan <shreyass@mit.edu>
2022-02-16 22:05:37 -0500

    Implement fi.bonds.yield_()
    Implement the yield function, and in doing so create the coupon_dates() function used implicitly in price().

--- src/fixedincome/bonds.py
+++ src/fixedincome/bonds.py
@@ -6,9 +6,11 @@ Implements various functions useful for bond analytics.
 
 
 import datetime
+from typing import List
+
 import dateutil.relativedelta
 import numpy as np
-
+import scipy.optimize
 
 from .utils import MONTHS_IN_YEAR
 from .utils import day_count_factor
@@ -52,6 +54,39 @@ def accrint(issue : datetime.date, first_interest : datetime.date, settlement : 
     return par * rate * _day_count_factor
 
 
+def coupon_dates(settlement : datetime.date, maturity : datetime.date, frequency : int) -> List[datetime.date]:
+    """
+    Returns the coupon dates for a bond from settlement to maturity.
+
+    Parameters
+    ----------
+    settlement : datetime.date
+        The bond's settlement date.
+    maturity : datetime.date
+        The bonds's maturity date (when it expires).
+    frequency : int
+        The number of coupon payments per year.
+
+    Returns
+    -------
+    list[datetime.date]
+        Coupon dates of the bond.
+    """
+
+    _coupon_dates = [maturity]
+
+    # Calculate length of coupon period in months
+    coupon_period = dateutil.relativedelta.relativedelta(months=-MONTHS_IN_YEAR//frequency)
+
+    # Calculate coupon dates backwards from maturity to issuance
+    while _coupon_dates[-1] > settlement:
+        _coupon_dates.append(_coupon_dates[-1] + coupon_period)
+    
+    # Return list in chronological order
+    _coupon_dates = _coupon_dates[::-1]
+    return _coupon_dates
+
+
 def price(settlement : datetime.date, maturity : datetime.date, rate : float, yld : float, redemption : float, frequency : int, basis : int = 0) -> float:
     """
     Returns the price per $100 face value of a bond.
@@ -87,12 +122,8 @@ def price(settlement : datetime.date, maturity : datetime.date, rate : float, yl
     _PAR = 100
 
     # Calculate coupon dates
-    coupon_dates = [maturity]
-    coupon_period = dateutil.relativedelta.relativedelta(months=-MONTHS_IN_YEAR//frequency)
-    while coupon_dates[-1] > settlement: # Calculate coupon dates backwards from maturity to issuance
-        coupon_dates.append(coupon_dates[-1] + coupon_period)
-    coupon_dates = coupon_dates[::-1]
-    num_periods = len(coupon_dates) - 1 # First coupon date is before settlement
+    _coupon_dates = coupon_dates(settlement=settlement, maturity=maturity, frequency=frequency)
+    num_periods = len(_coupon_dates) - 1 # First coupon date is before settlement
 
     # Calculate cash flows
     cash_flows = np.array([_PAR * rate / frequency] * num_periods) # Coupon payments
@@ -100,7 +131,7 @@ def price(settlement : datetime.date, maturity : datetime.date, rate : float, yl
 
     # Calculate discount rates
     discount_rates = np.array([1 + yld / frequency] * num_periods)
-    time_to_next = 1 - frequency * day_count_factor(start=coupon_dates[0], end=settlement, basis=basis, next_=coupon_dates[1], freq=frequency)
+    time_to_next = 1 - frequency * day_count_factor(start=_coupon_dates[0], end=settlement, basis=basis, next_=_coupon_dates[1], freq=frequency)
     discount_rate_powers = -1 * np.array([i + time_to_next for i in range(num_periods)])
     discount_factors = np.power(discount_rates, discount_rate_powers)
 
@@ -108,11 +139,50 @@ def price(settlement : datetime.date, maturity : datetime.date, rate : float, yl
     transaction_price = np.dot(cash_flows, discount_factors)
 
     # Calculate clean price
-    _accrint = accrint(issue=coupon_dates[0], first_interest=coupon_dates[1], settlement=settlement, rate=rate, par=_PAR, frequency=frequency, basis=basis)
+    _accrint = accrint(issue=_coupon_dates[0], first_interest=_coupon_dates[1], settlement=settlement, rate=rate, par=_PAR, frequency=frequency, basis=basis)
     clean_price = transaction_price - _accrint
     
     return clean_price
 
 
-def yield_():
-    pass
+def yield_(settlement : datetime.date, maturity : datetime.date, rate : float, price_ : float, redemption : float, frequency : int, basis : int = 0):
+    """
+    Returns the yield for a bond with $100 face value.
+
+    Parameters
+    ----------
+    settlement : datetime.date
+        The bond's settlement date.
+    maturity : datetime.date
+        The bonds's maturity date (when it expires).
+    rate : float
+        The bond's annual coupon rate.
+    price_ : float
+        The bond's quoted clean price.
+    redemption : float
+        The security's redemption value per $100 face value.
+    frequency : int
+        The number of coupon payments per year.
+    basis : int [optional]
+        The type of day count basis to use.
+            - 0 [default] : US (NASD) 30/360
+            - 1 : Actual/Actual
+            - 2 : Actual/360
+            - 3 : Actual/365
+            - 4 : European 30/360
+
+    Returns
+    -------
+    float
+        The annualized per-period bond yield.
+    """
+
+    _PAR = 100
+    _coupon_dates = coupon_dates(settlement=settlement, maturity=maturity, frequency=frequency)
+
+    # Special case for when no remaining coupons to be paid (only principal)
+    if len(_coupon_dates) == 1:
+        _day_count_factor = day_count_factor(start=issue, end=settlement, basis=basis, next_=first_interest, freq=frequency)
+        return ((redemption/100 + rate/frequency) - (_PAR/100 + (_day_count_factor * rate))) / (_PAR/100 + (_day_count_factor * rate)) * (frequency / (1 - _day_count_factor * frequency))
+    else: # Use optimization to identify the yield that matches quoted clean price
+        return scipy.optimize.newton(func=lambda y : price(settlement=settlement, maturity=maturity, rate=rate, yld=y, redemption=redemption, frequency=frequency, basis=basis) - price_, x0=rate, tol=0.0000001, maxiter=100)
